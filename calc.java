5addbce3c2027706a3fa98b6538404272872f71c
teamdreamrus <Atazhanov.biz@gmail.com>
2018-10-23 03:03:00 +0700

    Delete comments
    Delete comments

--- calc.java
+++ calc.java
@@ -37,35 +37,3 @@ class calc{
 
     }
 }
-/*import java.util.*;
-
-class calc{
-	public static void main(String[] arg){
-		double res;
-		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter a: ");
-		double a = scan.nextDouble();		
-		System.out.print("Enter b: ");
-		double b = scan.nextDouble();
-		System.out.print("Enter operation: ");
-		String op = scan.next();
-		switch(op){
-			case "+":
-				res = a + b;
-				break;
-			case "-":
-				res = a - b;
-				break;
-			case "*":
-				res = a * b;
-				break;
-			case "/":
-				res = a / b;
-				break;
-			default:
-				System.out.println("Operation is not correct");
-				return;		
-		}
-		System.out.println("Result = " + res);
-	}
-}*/
